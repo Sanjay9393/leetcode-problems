0535e13ba769f39b48d55688cfb3bef92b39b1f0
KUPPIREDDY SANJAY REDDY <24b11it057@adityauniversity.in>
2026-09-24 14:19:11 +0530

    Time: 1 ms (99.69%), Space: 45.3 MB (89.71%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int digsum(int n) {
+        int sum=0;
+        while(n>0){
+            sum+=n%10;
+            n/=10 ;
+        }
+        return sum ;
+    }
+    public int smallestIndex(int[] nums) {
+        int n = nums.length ;
+        for(int i=0 ; i<n; i++) {
+            int sum = digsum(nums[i]) ;
+            if(sum==i) return i ;
+        }
+        return -1 ;
+    }
+}
